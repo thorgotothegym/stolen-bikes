e405b1e54f7049039b24ce0871d18dbacf0bb3ef
BePandaMyFriend <al3ix.vidal@gmail.com>
2021-05-31 13:23:12 +0200

    feat: finish filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Layout, Row, Col, Button, Input } from "antd";
 import { Search } from "./component/search/search";
-import { useGetService } from "./hook/useGetService";
-import { apiStates, useApi } from "./hook/getApi";
 import { IncidentsResp, ResponseIncidents } from "./models/response";
 import { Reporter } from "./component/reporter/reporter";
 
@@ -27,43 +25,38 @@ function App() {
   const [loading, setLoading] = useState<boolean>(true);
   const [errorFetch, setErrorFetch] = useState<boolean | number>(false);
   // refactor useSTate data
-  const [data, setData] = useState<any>()
+  const [data, setData] = useState<any>([]);
 
-  const [query, setQuery] = useState<string>();
+  const [query, setQuery] = useState<string | any>();
 
-/*   const service = useGetService(
-    `https://bikewise.org:443/api/v2/incidents?page=1&incident_type=theft&proximity=52.52437%2C13.41053&proximity_square=100`
-  ); */
-
-  // This function is called when the input changes
   const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const enteredName = event.target.value;
     setQuery(enteredName);
   };
 
-  /*    // This function is triggered when the Search buttion is clicked
-   const search = () => {
-    const foundItems = PRODUCTS.filter((item) =>
-      item.name.toLowerCase().includes(query.toLowerCase())
+  const search = (stringToFind:string) => {
+    const toFind = data.filter((item: IncidentsResp) =>
+      item.title.includes(stringToFind)
     );
-    setResult(foundItems);
-  }; */
+    setData(toFind);
+  };
 
-  useEffect( () => {
+  useEffect(() => {
     const getData = async () => {
       const url = `https://bikewise.org:443/api/v2/incidents?page=1&incident_type=theft&proximity=52.52437%2C13.41053&proximity_square=100`;
       let response = await fetch(url);
 
-      if (response.status === 200 ) {
+      if (response.status === 200) {
         let data = await response.json();
-        setData(data);
+        setData(data.incidents);
         setLoading(false);
       } else {
-        alert('something was wrong with the API')
+        alert("something was wrong with the API");
       }
-    }
+    };
     getData();
-  },[])
+    console.log("data", data);
+  }, []);
 
   return (
     <div className="App">
@@ -118,6 +111,7 @@ function App() {
               placeholder="Search products"
               className="input"
             />
+            <button onClick={() => {search(query)}}>search</button>
           </Col>
         </Row>
         <Row>
@@ -126,11 +120,17 @@ function App() {
         <Content style={{ margin: "0 10px 0 10px", overflow: "" }}>
           <div className="site-layout-background">
             <div style={{ backgroundColor: "red" }}>
-              {loading === true ? <div>LOADING ...</div> : <pre>{data.incidents.map( (e:IncidentsResp) => {
-                return(
-                  <div>title: {e.title}</div>
-                )
-              })}</pre>}
+              {loading === true ? (
+                <div>LOADING ...</div>
+              ) : data !== "" ? (
+                <pre>
+                  {data.map((e: IncidentsResp) => {
+                    return <div>title: {e.title}</div>;
+                  })}
+                </pre>
+              ) : (
+                "empty"
+              )}
             </div>
           </div>
         </Content>
